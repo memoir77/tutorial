4bcc6cdf756c65ce298425e11930101b910c683b
first project <memoir77@naver.com>
2018-11-15 15:37:01 +0900

    first

---
+++ firstapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Curriculum(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '커리큘럼' + self.name
---
+++ firstapp/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from firstapp.models import Curriculum
+
+
+def index1(request):
+    return HttpResponse('<h1>Hello</h1>')
+
+def index2(request):
+    return HttpResponse('<h1>Hi</h1>')
+
+def main(request):
+    return HttpResponse('<h1>Main</h1>')
+
+def show(request):
+    curriculum = Curriculum.objects.all()
+    html = ''
+    for c in curriculum:
+        html += c.name + '<br>'
+    # return HttpResponse(html)
+
+    return render(
+        request, 'firstapp/show.html',{'data':html,'curriculum':curriculum})
